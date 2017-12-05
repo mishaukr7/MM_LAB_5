610a623d1a63ddf0c231575c2b78c4fc1bb4a454
Mykhailo Khomenko <mishaukr22@gmail.com>
2017-12-05 13:03:46 +0400

    end

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import analytical_solution
 from matplotlib import pyplot as plt
 import numerical_solution
 
+t0 = 20
+N0 = 44
+r = 1.58
+k = 78
+T = 40
+h = 0.1
+
+
 '''
 FREE analytical solution
 '''
-# x, y = analytical_solution.analytical_method_find_solution_free(20, 44, 1.58, 40)
+# x, y = analytical_solution.analytical_method_find_solution_free(t0, N0, r, T)
 # plt.plot(x, y)
 # plt.show()
 
@@ -13,20 +21,20 @@ FREE analytical solution
 '''
 LIMITED analytical solution
 '''
-#x, y = analytical_solution.analytical_method_find_solution_limited(20, 44, 1.58, 78, 40)
+#x, y = analytical_solution.analytical_method_find_solution_limited(t0, N0, r, k, T)
 #plt.plot(x, y)
 #plt.show()
 
 '''
 FREE numerical solution
 '''
-# x, y = numerical_solution.numerical_solution_free(20, 44, 1.58, 78, 40, 0.1, numerical_solution.f_free)
+# x, y = numerical_solution.numerical_solution_free(t0, N0, r, k, T, h, numerical_solution.f_free)
 # plt.plot(x, y)
 # plt.show()
 
 '''
 LIMITED numerical solution
 '''
-# x, y = numerical_solution.numerical_solution_free(20, 44, 1.58, 78, 40, 0.1, numerical_solution.f_limited)
+# x, y = numerical_solution.numerical_solution_free(t0, N0, r, k, T, h, numerical_solution.f_limited)
 # plt.plot(x, y)
 # plt.show()
